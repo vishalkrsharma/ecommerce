9ca55e396fc9c64ef8be4d05f1a089f6f68bcd7b
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-04 22:36:38 +0530

    router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,23 @@
-import data from './data';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className='App'>
-      <header>
-        <a href='/'>ecommerce</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className='product-info'>
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-              </div>
-              <p>
-                <stong>₹{product.price}</stong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <header>
+          <Link to='/'>ecommerce</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
