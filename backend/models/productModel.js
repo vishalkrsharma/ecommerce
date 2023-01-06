1df2ec2924b083213bbeb56d9fbc0cbc8cd1e2eb
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-06 22:48:55 +0530

    user added

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -12,13 +12,34 @@ const productSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    image: { type: String, required: true },
-    brand: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    countInStock: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    numReviews: { type: Number, required: true },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+    },
   },
   {
     timestamps: true,
