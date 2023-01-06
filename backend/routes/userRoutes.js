d2b574f012892853b093336228da13f8c0bfb4a7
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-06 23:23:12 +0530

    signin api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,29 @@
 import express from 'express';
-import data from '../data.js';
-import Product from '../models/productModel.js';
-const seedRouter = express.Router();
+import bcrypt from 'bcryptjs';
+import expressAsyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
 
-seedRouter.get('/', async (req, res) => {
-  await Product.remove({});
-  const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
-});
+const userRouter = express.Router();
 
-export default seedRouter;
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+export default userRouter;
