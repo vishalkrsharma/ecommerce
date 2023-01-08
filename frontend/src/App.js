2035f7882fa3633185266685d31021bc96d07203
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-08 23:28:55 +0530

    sign up dont

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import SigninScreen from './screens/SigninScreen';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ShippingScreen from './screens/ShippingScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -69,6 +70,7 @@ function App() {
               <Route path='/' element={<HomeScreen />} />
               <Route path='/cart' element={<CartScreen />} />
               <Route path='/signin' element={<SigninScreen />} />
+              <Route path='/signup' element={<SignupScreen />} />
               <Route path='/shipping' element={<ShippingScreen />} />
             </Routes>
           </Container>
